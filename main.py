1bb0a6fe4b2c60f3b9e2bbd7b4428040f4f33e52
sahil-v001 <sahilvermatokyo1@gmail.com>
2026-05-20 15:59:44 +0530

    updated the main file
    Before i was not validating the input , now we used pydantic , to validate

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
-app = FastAPI() 
+class Post(BaseModel):
+  title : str
+  content : str
+  published : bool = True
+  rating : int = 0
 
-@app.get("/")
+app = FastAPI() # instance of fast api
+
+@app.get("/") # here get is just one of the HTTP method
 async def root():
-  return {"message" : "Hello Sahil"}  
+  return {"message" : "Hello Sahil"}  # fast api convert this into a json , whichis the universal for wev dev
 
 @app.get("/post")
 async def get_post():
   return {"post" : "this is yout post"}
 
 @app.post("/createpost")
-async def Create_Post(payload: dict = Body(...)):
-  print(payload)
-  return {"post created" : f" title : {payload['title']} , content : {payload['content']}"}
+async def Create_Post(new_post : Post): # i used pydantic here , i mean its a kind of checkup on the recived data
+  print(new_post.rating)
+  return {"data" : "new post"}
